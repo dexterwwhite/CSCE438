ca75ef3d2608b3ab38649cedbc84e788140db30f
dexterwwhite <dexterwwhite@tamu.edu>
2022-01-28 22:36:37 -0600

    Basic Communication

--- MP1/crsd.cpp
+++ MP1/crsd.cpp
@@ -13,12 +13,97 @@
 #include <string.h>
 #include "interface.h"
 
-using std::cout, std::endl, std::string, std::thread;
+using std::cout, std::endl, std::string, std::thread, std::vector;
+
+struct room {
+    public:
+        string name;
+        int num_members;
+        int port_no;
+        room(string roomName, int port)
+        {
+            name = roomName;
+            num_members = 0;
+            port_no = port;
+        }
+};
+
+vector<room> rooms;
+int currentPort = 1500;
+
+void process_request(int sockfd, char* buffer)
+{
+    Reply* serverReply = new Reply;
+    if(buffer[0] == 'c')
+    {
+        string name = "";
+        for(int i = 1; buffer[i] != '\0'; i++)
+        {
+            name += buffer[i];
+        }
+        for(int i = 0; i < rooms.size(); i++)
+        {
+            if(name == rooms.at(i).name)
+            {
+                serverReply->status = FAILURE_ALREADY_EXISTS;
+                if(send(sockfd, (char*)serverReply, sizeof(Reply), 0) != 0)
+                    perror("Server send");
+                return;
+            }
+        }
+
+        room newRoom(name, currentPort);
+        rooms.push_back(newRoom);
+        cout << "Room created!" << endl;
+        serverReply->status = SUCCESS;
+        if(send(sockfd, (char*)serverReply, sizeof(Reply), 0) != 0)
+            perror("Server send");
+        return;
+    }
+    else if(buffer[0] == 'd')
+    {
+        cout << "OKAY :(" << endl;
+    }
+    else if(buffer[0] == 'j')
+    {
+        cout << "OKAY :(" << endl;
+    }
+    else if(buffer[0] == 'l')
+    {
+        cout << "OKAY :(" << endl;
+    }
+    else
+    {
+        cout << "OKAY :(" << endl;
+    }
+}
 
 void handle_client(int sockfd) {
     cout << "OKAY" << endl;
     int bufferCapacity = 256;
     char* buffer = new char[bufferCapacity];
+	if (!buffer){
+        cout << "Cannot allocate memory for server buffer" << endl;
+        exit(1);
+	}
+	while (true){
+		int nbytes = recv(sockfd, buffer, bufferCapacity, 0);
+		if (nbytes < 0){
+			cout << "Client-side terminated abnormally" << endl;
+			break;
+		}else if (nbytes == 0){
+			// could not read anything in current iteration
+			cout << "Could not read anything" << endl;
+			break;
+		}
+		// MESSAGE_TYPE m = *(MESSAGE_TYPE *) buffer;
+		// if (m == QUIT_MSG){
+		// 	break;
+		// 	// note that QUIT_MSG does not get a reply from the server
+		// }
+		process_request(sockfd, buffer);
+	}
+	delete [] buffer;
     close(sockfd);
 }
 
@@ -26,10 +111,11 @@ void processing_loop(int sockfd)
 {
     while(true)
     {
+        cout << "Entered PL" << endl;
         struct sockaddr_storage otherAddr;
         socklen_t size = sizeof(otherAddr);
         int newSockFD;
-        if((newSockFD = accept(sockfd, (struct sockaddr *) &otherAddr, &size)) != 0)
+        if((newSockFD = accept(sockfd, (struct sockaddr *) &otherAddr, &size)) == -1)
         {
             perror("Server accept");
         }
@@ -48,6 +134,8 @@ int main(int argc, char** argv)
 
     string this_port = argv[1];
     cout << "Argv[1]: " << this_port << endl;
+    currentPort = atoi(argv[1]);
+    currentPort++;
 
     struct sockaddr_storage their_addr;
     socklen_t addr_size;
@@ -93,10 +181,10 @@ int main(int argc, char** argv)
 
     // now accept an incoming connection:
 
-    addr_size = sizeof their_addr;
-    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+    // addr_size = sizeof their_addr;
+    // new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
 
-    cout << "PASSED accept" << endl;
+    // cout << "PASSED accept" << endl;
 
     // ready to communicate on socket descriptor new_fd!
 
