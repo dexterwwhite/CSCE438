98258ff5082f3dc92f994b8acbcafd7d4102ea55
dexterwwhite <dexterwwhite@tamu.edu>
2022-02-28 15:39:30 -0600

    Client code refactor

--- MP2/tsc.cc
+++ MP2/tsc.cc
@@ -91,15 +91,16 @@ int Client::connectTo()
     // a member variable in your own Client class.
     // Please refer to gRpc tutorial how to create a stub.
 	// ------------------------------------------------------------
+    
+    //Sets up client stub
     string param = hostname + ":" + port;
     this->stub_ = (SNSService::NewStub(grpc::CreateChannel(param, grpc::InsecureChannelCredentials())));
 
+    //Client calls "Login" service
     ClientContext cc;
     Reply rep;
     Request req;
     req.set_username(username);
-    //req.set_arguments(1, "LOGIN");
-
     Status status = stub_->Login(&cc, req, &rep);
 
     if(status.ok())
@@ -123,10 +124,13 @@ IReply Client::processCommand(std::string& input)
     // TIMELINE
 	//
 	// ------------------------------------------------------------
-	IReply ire;
-    
+	
+    //Variable set up
+    IReply ire;
     string cmd = "";
     int i;
+
+    //Parses input for the actual command (separate from its argument)
     for(i = 0; i < input.length(); i++)
     {
         if(input.at(i) == ' ')
@@ -136,25 +140,29 @@ IReply Client::processCommand(std::string& input)
     
     if(cmd == "FOLLOW")
     {
+        //Parses input for its argument (aka the user to be followed)
         string fUser = "";
         for(int i = 7; i < input.length(); i++)
         {
             fUser += input.at(i);
         }
 
+        //Client stubs calls "Follow" service
         ClientContext sc;
         Reply rep;
         Request req;
         req.set_username(username);
         req.add_arguments(fUser);
-
         Status status = stub_->Follow(&sc, req, &rep);
 
+        //Handling of the different results
         if(status.ok())
         {
+            //I chose to have any error be present in the "all_users" data variable within Reply
             ire.grpc_status = status;
             if(rep.all_users_size() > 0)
             {
+                //Due to the above reasoning, these are the two failure cases
                 if(rep.all_users(0) == "failed-follows")
                 {
                     ire.comm_status = FAILURE_ALREADY_EXISTS;
@@ -168,37 +176,43 @@ IReply Client::processCommand(std::string& input)
             }
             else
             {
+                //Successful call to Follow
                 ire.comm_status = SUCCESS;
                 return ire;
             }
         }
         else
         {
+            //gRPC status did not return successful, so this was a gRPC error and not a user error
             ire.grpc_status = status;
             return ire;
         }
     }
     else if(cmd == "UNFOLLOW")
     {
+        //Parses input for its argument (aka the user to be unfollowed)
         string fUser = "";
         for(int i = 9; i < input.length(); i++)
         {
             fUser += input.at(i);
         }
 
+        //Client stub calls "Unfollow" service
         ClientContext sc;
         Reply rep;
         Request req;
         req.set_username(username);
         req.add_arguments(fUser);
-
         Status status = stub_->UnFollow(&sc, req, &rep);
 
+        //Handles different situations based on server response
         if(status.ok())
         {
+            //I chose to have any error be present in the "all_users" data variable within Reply
             ire.grpc_status = status;
             if(rep.all_users_size() > 0)
             {
+                //Due to the above reasoning, these are the 3 failure cases of unfollow
                 if(rep.all_users(0) == "failed-notfollow")
                 {
                     ire.comm_status = FAILURE_INVALID_USERNAME;
@@ -217,60 +231,68 @@ IReply Client::processCommand(std::string& input)
             }
             else
             {
+                //Successful call to Unfollow
                 ire.comm_status = SUCCESS;
                 return ire;
             }
         }
         else
         {
-            cout << status.error_code() << endl;
-            cout << status.error_message() << endl;
+            //gRPC status did not return successful, so this was a gRPC error and not a user error
             ire.grpc_status = status;
             return ire;
         }
     }
     else if(input == "LIST")
     {
+        //Client stub calls "List" service
         ClientContext cc;
         Reply rep;
         Request req;
         req.set_username(username);
-        //req.set_arguments(1, "LOGIN");
-
         Status status = stub_->List(&cc, req, &rep);
+
+        //List can not fail other than by gRPC error, so there are no failure cases to handle
         if(status.ok())
         {
-            vector<string> all;
-            vector<string> following;
             ire.grpc_status = status;
             ire.comm_status = SUCCESS;
+
+            //Populate IReply all users list
+            vector<string> all;
             for(int i = 0; i < rep.all_users_size(); i++)
             {
                 all.push_back(rep.all_users(i));
             }
 
+            //Populate IReply following users list
+            vector<string> following;
             for(int i = 0; i < rep.following_users_size(); i++)
             {
                 following.push_back(rep.following_users(i));
             }
+
             ire.all_users = all;
             ire.following_users = following;
             return ire;
         }
         else
         {
+            //gRPC status did not return successful, so this was a gRPC error and not a user error
             ire.grpc_status = status;
             return ire;
         }
     }
     else if(input == "TIMELINE")
     {
+        //Client does not actually call "Timeline" from here, it is called later in the processTimeline function
         ire.grpc_status = Status::OK;
         ire.comm_status = SUCCESS;
         return ire;
     }
     else
     {
+        //If there is an invalid input sent by the client that is not caught by getCommand() function
         ire.grpc_status = Status::OK;
         ire.comm_status = FAILURE_INVALID;
         return ire;
@@ -320,13 +342,19 @@ IReply Client::processCommand(std::string& input)
     // For the command "LIST", you should set both "all_users" and 
     // "following_users" member variable of IReply.
     // ------------------------------------------------------------
-    
-    //IReply ire;
     return ire;
 }
 
+/**
+* This function is meant to be used by a separate thread that's sole purpose is 
+* to read any messages the server sends over. Due to the multithreaded nature of 
+* the client, it does not have to worry about blocking while waiting for messages.
+*
+* @param crw the ClientReaderWriter that is used for reading messages from the server
+*/
 void timelineThread(std::shared_ptr<ClientReaderWriter<Message, Message>> crw)
 {
+    //Infinite loops and reads/displays any incoming messages from the server
     while(true)
     {
         Message post;
@@ -339,95 +367,7 @@ void timelineThread(std::shared_ptr<ClientReaderWriter<Message, Message>> crw)
 
 void Client::processTimeline()
 {
-    ClientContext cc;
-    std::shared_ptr<ClientReaderWriter<Message, Message>> crw(stub_->Timeline(&cc));
-    Message m1;
-    m1.set_username(username);
-    string mes1 = "conn-establish";
-    m1.set_msg(mes1);
-    crw->Write(m1);
-    //crw->WritesDone();
-    
-    // Status status = crw->Finish();
-    // cout << "MAde it here3" << endl;
-
-    Message tlmsg;
-    while(crw->Read(&tlmsg))
-    {
-        if(tlmsg.username() != "server--end**")
-        {
-            cout << tlmsg.username();
-            cout << tlmsg.msg();
-            cout << endl;
-        }
-        else
-        {
-            break;
-        }
-    }
-    //status = crw->Finish();
-
-    // //Status status = stub_->Timeline(&cc, &crw);
-    // Message m1;
-    // m1.set_username(username);
-    // string messo = "Hi friend!";
-    // m1.set_msg(messo);
-
-    // Timestamp ts = google::protobuf::util::TimeUtil::GetCurrentTime();
-    // //ts->GetCurrentTime();
-    // //Timestamp ts = google::protobuf::util::time_util::TimeTToTimestamp(rawtime);
-    // m1.set_allocated_timestamp(&ts);
-
-    // crw->Write(m1);
-
-    vector<thread> receiver;
-    receiver.emplace_back(timelineThread, crw);
-
-    //thread receiver(Client::timelineThread, crw);
-
-    // int fd = 0;
-    // struct pollfd fds[1];
-    // int timeout, pollval;
-
-    cout << "Made it to here" << endl;
-    while(true)
-    {
-        cout << "Entered loop!" << endl;
-        // fds[0].fd = fd;
-        // fds[0].events = 0;
-        // fds[0].events |= POLLIN;
-        // timeout = 1000;
-
-        // pollval = poll(fds, 1, timeout);
-
-        // if(pollval == 0)
-        // {
-        //     // cout << "READ!" << endl;
-        //     // Message post;
-        //     // if(crw->Read(&post))
-        //     // {
-        //     //     cout << post.username() << post.msg() << endl;
-        //     // }
-        //     cout << "";
-        // }
-        // else
-        // {
-        Message loopmsg;
-        string myMsg = getPostMessage();
-        loopmsg.set_username(username);
-        loopmsg.set_msg(myMsg);
-        Timestamp ts = google::protobuf::util::TimeUtil::GetCurrentTime();
-        loopmsg.set_allocated_timestamp(&ts);
-        crw->Write(loopmsg);
-        loopmsg.release_timestamp();
-        if(loopmsg.has_timestamp())
-        {
-            loopmsg.clear_timestamp();
-        }
-        //crw->WritesDone();
-        //}
-    }
-	// ------------------------------------------------------------
+    // ------------------------------------------------------------
     // In this function, you are supposed to get into timeline mode.
     // You may need to call a service method to communicate with
     // the server. Use getPostMessage/displayPostMessage functions
@@ -443,4 +383,56 @@ void Client::processTimeline()
     // and you can terminate the client program by pressing
     // CTRL-C (SIGINT)
 	// ------------------------------------------------------------
+
+    //Sets up ClientReaderWriter with a call to "Timeline" service
+    ClientContext cc;
+    std::shared_ptr<ClientReaderWriter<Message, Message>> crw(stub_->Timeline(&cc));
+
+    //Initial message that lets server know who is making the Timeline connection
+    Message m1;
+    m1.set_username(username);
+    string mes1 = "conn-establish";
+    m1.set_msg(mes1);
+    crw->Write(m1);
+
+    //Reads and displays any past timeline posts on the user's timeline
+    Message tlmsg;
+    while(crw->Read(&tlmsg))
+    {
+        //Server sends a message "server--end**" when it is done sending past timeline posts
+        if(tlmsg.username() != "server--end**")
+        {
+            cout << tlmsg.username();
+            cout << tlmsg.msg();
+            cout << endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    //Creates a new thread for above function, timelineThread
+    vector<thread> receiver;
+    receiver.emplace_back(timelineThread, crw);
+
+    //Infinite loops and sends any user posts taken from standard input
+    while(true)
+    {
+        //Reads in from standard input, sets up loopmsg with user post information, and sends to server
+        Message loopmsg;
+        string myMsg = getPostMessage();
+        loopmsg.set_username(username);
+        loopmsg.set_msg(myMsg);
+        Timestamp ts = google::protobuf::util::TimeUtil::GetCurrentTime();
+        loopmsg.set_allocated_timestamp(&ts);
+        crw->Write(loopmsg);
+
+        //Prevents memory leaks from the timestamp
+        loopmsg.release_timestamp();
+        if(loopmsg.has_timestamp())
+        {
+            loopmsg.clear_timestamp();
+        }
+    }
 }
