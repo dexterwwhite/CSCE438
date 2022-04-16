51c567d6eebd808a28b7af55dd08f51fd24dc18f
Dexter White <dexter.w.white12@gmail.com>
2022-04-16 15:51:01 -0500

    Added Startup Script

---
+++ MP3/startup.sh
@@ -0,0 +1,17 @@
+#Start up Coordinator
+./coordinator -p 1234 &
+
+#Start up server cluster 1
+./tsd -h localhost -c 1234 -p 1235 -i 1 -t master &
+./tsd -h localhost -c 1234 -p 1236 -i 1 -t slave &
+#./synchronizer
+
+#Start up server cluster 2
+./tsd -h localhost -c 1234 -p 1237 -i 2 -t master &
+./tsd -h localhost -c 1234 -p 1238 -i 2 -t slave &
+#./synchronizer
+
+#Start up server cluster 3
+./tsd -h localhost -c 1234 -p 1239 -i 3 -t master &
+./tsd -h localhost -c 1234 -p 1240 -i 3 -t slave &
+#./synchronizer
